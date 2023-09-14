9795db0b78993e1fc2f5ba4a5976193c54075fa6
kokkilE <kkuldak1@naver.com>
2023-09-14 15:32:44 +0900

    [Gold II] Title: 후위 표기식, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Gold/1918. 후위 표기식/후위 표기식.swift
@@ -0,0 +1,71 @@
+import Foundation
+
+func operatorPriority(_ data: Character, isInParenthesis: Int) -> Int {
+    var priority = 0
+    
+    switch data {
+    case "/", "*":
+        priority += 2
+    case "+", "-":
+        priority += 1
+    default:
+        break
+    }
+    
+    return priority + isInParenthesis
+}
+
+let expression = readLine()!.map({$0})
+var operandStack = [Character]()
+var operatorStack = [Character]()
+var operatorPriorityStack = [Int]()
+
+var postfixExpression = ""
+var isInParenthesis = 0
+
+expression.forEach {
+    switch $0 {
+    case "+", "-", "/", "*":
+        // 연산자 스택이 비어있으면 연산하지 않음
+        if operatorPriorityStack.isEmpty {
+            operatorStack.append($0)
+            operatorPriorityStack.append(operatorPriority($0, isInParenthesis: isInParenthesis))
+        }
+        // 현재 연산 우선순위가 기존보다 낮으면 기존의 연산자를 연산
+        else if operatorPriority($0, isInParenthesis: isInParenthesis) <= operatorPriorityStack.last! {
+            while let priority = operatorPriorityStack.last,
+                  operatorPriority($0, isInParenthesis: isInParenthesis) <= priority {
+                
+                while !operandStack.isEmpty {
+                    postfixExpression.append(operandStack.removeFirst())
+                }
+                
+                postfixExpression.append(operatorStack.removeLast())
+                operatorPriorityStack.popLast()
+            }
+            
+            operatorStack.append($0)
+            operatorPriorityStack.append(operatorPriority($0, isInParenthesis: isInParenthesis))
+        } else {
+            operatorStack.append($0)
+            operatorPriorityStack.append(operatorPriority($0, isInParenthesis: isInParenthesis))
+        }
+        
+    case "(":
+        isInParenthesis += 2
+    case ")":
+        isInParenthesis -= 2
+    default:
+        operandStack.append($0)
+    }
+}
+
+while !operandStack.isEmpty {
+    postfixExpression.append(operandStack.removeFirst())
+}
+
+while !operatorStack.isEmpty {
+    postfixExpression.append(operatorStack.removeLast())
+}
+
+print(postfixExpression)
